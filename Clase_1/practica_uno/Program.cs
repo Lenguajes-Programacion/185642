8a6a8243a357594839493ca78d15993dc4c1c074
Andres Noriega <60111218+AndyNorc5@users.noreply.github.com>
2020-01-22 12:59:16 -0700

    Hola mundo

---
+++ Clase_1/practica_uno/Program.cs
@@ -0,0 +1,13 @@
+using System;
+//Importamos la libreria principal
+
+namespace practica_uno
+{ 
+    class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
