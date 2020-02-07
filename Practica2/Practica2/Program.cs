60b95f6e9e294c76bc5706db49688f115253b308
JOSE ANDRES NORIEGA CONTRERAS <al185642@alumnos.uacj.mx>
2020-02-07 12:52:18 -0700

    pract2

---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch(args[1])
+            {
+                case"+":
+                    Console.WriteLine("Es una suma");
+                    break;
+                default:
+                    Console.WriteLine("Los argumentos no cuentan con un operador valido:" + args[1]);
+                    break;
+            }
+        }
+    }
+}
