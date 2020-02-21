4f2e515959e98f43e72dbb3cc4c15fe827b2173c
JOSE ANDRES NORIEGA CONTRERAS <andres.noriegacon@gmail.com>
2020-02-21 12:49:51 -0700

    Memoria

---
+++ Calculadora/P2/P2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Json.Ling;
+using System;
+using System.IO;
+
+
+namespace P2
+{
+    class Memoria
+    {
+        public void LeerMemoria()
+        {
+            string archivoDB = "../../;
+            StreamReader reader = StreamReader(archivoDB);
+            var dbJSON = reader.ReadToEnd();
+            var 
+        }
+    }
+}
